c81a2049afe1ff6c062363d35908aca865d85a6a
hsoo6373 <hrithviksood1@gmail.com>
2020-06-11 08:35:23 +1000

    Added paths to home, about, contact and work pages

--- portfolio_website/urls.py
+++ portfolio_website/urls.py
@@ -2,10 +2,7 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -14,8 +11,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from pages import views
 
 urlpatterns = [
+	path('', views.home, name='home'),
+	path('about/', views.home, name='about'),
+	path('work/', views.work, name='work'),
+	path('blog/', views.blog, name='blog'),
+	path('contact/', views.contact, name='contact'),
+	path('', views.home, name='email'),
+	path('', views.home, name='linkedin'),
+	path('', views.home, name='github'),
+	path('', views.home, name='bitbucket'),
+	path('', views.home, name='instagram'),
     path('admin/', admin.site.urls),
 ]
