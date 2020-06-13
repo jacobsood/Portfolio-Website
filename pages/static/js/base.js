ee8c2087eda55b283e9c1706c49c7bc263d422b8
hsoo6373 <hrithviksood1@gmail.com>
2020-06-13 13:00:33 +1000

    Background colour changes on title hover. Landing page moves up on click

--- pages/static/js/base.js
+++ pages/static/js/base.js
@@ -1,25 +1,48 @@
+/* Script relating to the header */
+// Change to enum when using typescript (or vue)
+const bgColours = ["#F0EBF4", "#B39BC8", "#FFD8CE", "#F76C6C",
+  "#F0DEFF", "#FEADB9"];
+const bwColour = ["white", "black"];
+var bgSize = 2;
+var bgIndex = 0;
+
+const pageHeader = document.getElementById("page_header");
+
+// update background colour
+// parameter: element
+function updateBg(element) {
+  element.style.backgroundColor = bwColour[bgIndex];
+  bgIndex = (++bgIndex >= bgSize ? 0 : bgIndex);
+}
+
 /* Script related to the landing page */
-const landing_div = document.getElementById("landing_div");
+const landingDiv = document.getElementById("landing_div");
+const typedJsDiv = document.getElementById("typed_js");
 const body = document.body;
 
-// On hover of text, flip background and foreground colour
-landing_div.addEventListener("mouseenter", e => {
-  landing_div.style.color = "black";
-  body.style.backgroundColor = "white";
+// On hover, flip background and foreground colour
+typedJsDiv.addEventListener("mouseenter", e => {
+  updateBg(landingDiv);
+  landingDiv.style.color = "black";
 });
 
-landing_div.addEventListener("mouseleave", e => {
-  landing_div.style.color = "white";
-  body.style.backgroundColor = "black";
-})
+typedJsDiv.addEventListener("mouseleave", e => {
+  updateBg(landingDiv);
+  landingDiv.style.color = "white";
+});
 
 // Using typed.js for the landing page name.
 var typed = new Typed("#name_typed", {
-  strings: ["Hrithvik S", "Jacob Sood^5000", "Hrithvik Sood"],
-  typeSpeed: 100,
+  strings: ["Hrithvik S", "Jacob Sood^4000", "Hrithvik Sood"],
+  typeSpeed: 85,
   backSpeed: 100,
   loop: false,
   startDelay: 1000,
   backDelay: 0,
   showCursor: [true, "HELLO", true]
+});
+
+// Move down from landing page into the index
+landingDiv.addEventListener("click", e => {
+  pageHeader.scrollIntoView({behavior: "smooth"});
 });
