5e76070865fea51e27154d7f07a8669f01480121
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-06-20 04:54:03 +0000

    Changed allowed hosts

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["13.236.90.137"]
+ALLOWED_HOSTS = ["3.105.120.10", "hrithviksood.me"]
 
 
 # Application definition
