1c980b92d164b4cd08a5e7088ddb64ba15c5f435
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-06-22 08:15:25 +0000

    Background colour changes when hovering on the header or footer links. Fixed runtime error - script was using element which did not exist in one of the pages.

--- pages/static/js/base.js
+++ pages/static/js/base.js
@@ -1,5 +1,4 @@
 /* Global */
-
 const body = document.body;
 const pageHeader = document.getElementById("page_header");
 
@@ -9,78 +8,104 @@ const pageHeader = document.getElementById("page_header");
 const bgColours = ["#F0EBF4", "#B39BC8", "#FFD8CE", "#F76C6C",
   "#F0DEFF", "#FEADB9"];
 const bwColour = ["white", "black"];
-var bgSize = 2;
+const whiteShades = ["#FFFAFA", "#F0FFF0", "#F5FFFA", "#F0F8FF", "#F8F8FF", "#F5F5F5"];
+const landingBgSize = 2;
+const bgSize = 6;
+var landingIndex = 0;
 var bgIndex = 0;
 
 // update background colour
 // parameter: element
-function updateBg(elements) {
-  for (var i = 0; i < elements.length; i++)
-    elements[i].style.backgroundColor = bwColour[bgIndex];
-  bgIndex = (++bgIndex >= bgSize ? 0 : bgIndex);
+function updateBg(element, colourScheme) {
+  if (colourScheme === "bw") {
+    element.style.backgroundColor = bwColour[landingIndex];
+    landingIndex = (++landingIndex >= landingBgSize ? 0 : landingIndex);
+  }
+  else if (colourScheme === "whiteShades") {
+    element.style.backgroundColor = whiteShades[bgIndex];
+    bgIndex = (++bgIndex >= bgSize ? 0 : bgIndex);
+  }
+}
+
+function bgReset() {
+  body.style.backgroundColor = "#F0FFFF";
 }
 
 /* Script related to the landing page */
-
 const typedJsContainer = document.getElementById("typedjs_container");
 const typedJsDiv = document.getElementById("typed_js");
 const comingSoon = document.getElementById("coming_soon");
 
 // On hover, flip background and foreground colour
-typedJsDiv.addEventListener("mouseenter", e => {
-  updateBg([typedJsContainer]);
-  typedJsContainer.style.color = "black";
-});
-
-typedJsDiv.addEventListener("mouseleave", e => {
-  updateBg([typedJsContainer]);
-  typedJsContainer.style.color = "white";
-});
-
-// Using typed.js for the landing page name.
-var typed = new Typed("#name_typed", {
-  strings: ["Hrithvik S", "Jacob Sood^4000", "Hrithvik Sood"],
-  typeSpeed: 85,
-  backSpeed: 100,
-  loop: false,
-  startDelay: 1000,
-  backDelay: 0,
-  showCursor: true,
-  cursorChar: "|",
-  onTypingResumed: function(pos, typed) { 
-    setTimeout(function() {
-      comingSoon.classList.add("fade_in");
-      b.reveal(10000, 2000);
-    }, 2750);
-  }
-});
-
-// Coming soon baffle
-let b = baffle("#coming_soon")
-  .start()
-  .set({
-    characters: "█<█ ▒▓/▓▓ ▒>▓<▓ ░█▓ ▒█>░▓ ▓██░ ▒▓░ ▓/█▒ >/░▒",
-    speed: 200,
+if (typedJsDiv) {
+  typedJsDiv.addEventListener("mouseenter", e => {
+    updateBg(typedJsContainer, "bw");
+    typedJsContainer.style.color = "black";
   });
 
-// Move down from landing page into the index
-typedJsContainer.addEventListener("click", e => {
-  pageHeader.scrollIntoView({behavior: "smooth"});
-});
+  typedJsDiv.addEventListener("mouseleave", e => {
+    updateBg(typedJsContainer, "bw");
+    typedJsContainer.style.color = "white";
+  });
+  
+  // Using typed.js for the landing page name.
+  var typed = new Typed("#name_typed", {
+    strings: ["Hrithvik S", "Jacob Sood^4000", "Hrithvik Sood"],
+    typeSpeed: 85,
+    backSpeed: 100,
+    loop: false,
+    startDelay: 1000,
+    backDelay: 0,
+    showCursor: true,
+    cursorChar: "|",
+    onTypingResumed: function(pos, typed) { 
+      setTimeout(function() {
+        comingSoon.classList.add("fade_in");
+        b.reveal(10000, 2000);
+      }, 2750);
+    }
+  });
+  
+  // Coming soon baffle
+  let b = baffle("#coming_soon")
+    .start()
+    .set({
+      characters: "█<█ ▒▓/▓▓ ▒>▓<▓ ░█▓ ▒█>░▓ ▓██░ ▒▓░ ▓/█▒ >/░▒",
+      speed: 200,
+    });
 
-const content = document.getElementById("content");
-const footer = document.getElementById("page_footer");
-const header = document.getElementById("page_header");
-const footLinks = document.getElementsByClassName("sm");
-
-const len = footLinks.length;
-for (var i = 0; i < len; i++) {
-  footLinks[i].addEventListener("mouseenter", e => {
-    updateBg([content, footer, header]);
+  // Move down from landing page into the home page and hide typed js
+  typedJsContainer.addEventListener("click", e => {
+    pageHeader.scrollIntoView({behavior: "smooth"});
+    setTimeout(function() {
+      typedJsContainer.style.display = "none";
+    }, 600);
   });
 }
-for (var i = 0; i < len; i++) {
-  footLinks[i].addEventListener("mouseleave", e => {
-    updateBg([content, footer, header]);
+
+
+
+/* Script related to the header and footer */
+const menu = document.getElementsByClassName("menu");
+const socialMedia = document.getElementsByClassName("social_media");
+
+const headerLen = menu.length;
+const footerLen = socialMedia.length;
+
+for (var i = 0; i < footerLen; i++) {
+  socialMedia[i].addEventListener("mouseenter", e => {
+    updateBg(body, "whiteShades");
   });
+}
+for (var i = 0; i < footerLen; i++) {
+  socialMedia[i].addEventListener("mouseleave", bgReset);
+}
+
+for (var i = 0; i < headerLen; i++) {
+  menu[i].addEventListener("mouseenter", e => {
+    updateBg(body, "whiteShades");
+  });
+}
+for (var i = 0; i < headerLen; i++) {
+  menu[i].addEventListener("mouseleave", bgReset);
 }
