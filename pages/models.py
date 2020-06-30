aea2a8a12757d8b61be3b56faabe03fb6ff02158
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-06-30 07:08:03 +0000

    Fixed syntax errors

--- pages/models.py
+++ pages/models.py
@@ -1,12 +1,12 @@
 from djongo import models
 from django import forms
 
-class Tool(models.Model) {
+class Tool(models.Model):
     tool = models.CharField(max_length=30)
     
     class Meta:
         abstract = True
-}
+
 
 class ToolForm(forms.ModelForm):
     class Meta:
@@ -15,12 +15,12 @@ class ToolForm(forms.ModelForm):
             'tool'
         )
 
-class Description(models.Model) {
+class Description(models.Model):
     description = models.TextField()
     
     class Meta:
         abstract = true
-}
+
 
 class DescriptionForm(forms.ModelForm)
     class Meta:
@@ -29,22 +29,22 @@ class DescriptionForm(forms.ModelForm)
             'description'
         )
     
-class Links(models.Model) {
+class Links(models.Model):
     link = models.URLField()
     
     class Meta:
         abstract = true
-}
 
-class LinksForm(forms.ModelForm) {
+
+class LinksForm(forms.ModelForm):
     class Meta:
         model = Links
         field = (
             'link'
         )
-}
 
-class Blog(models.Model) {
+
+class Blog(models.Model):
     title = models.CharField(max_length=255)
     date = models.DateField(auto_now=False, auto_now_add=False)
     location = models.CharField(max_length=40)
@@ -60,9 +60,9 @@ class Blog(models.Model) {
     )
     
     objects = models.DjongoManager()
-}
 
-class Work(models.Model) {
+
+class Work(models.Model):
     title = models.CharField(max_length=255)
     purpose = models.TextField()
     
@@ -82,12 +82,10 @@ class Work(models.Model) {
     )
     
     objects = models.DjongoManager()
-}
 
-class About(models.Model) {
-    
-}
 
-class Index(models.Model) {
-    
-}
+class About(models.Model):
+    pass
+
+class Index(models.Model):
+    pass
