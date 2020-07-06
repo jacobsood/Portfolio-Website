106876b6d13eac9e0dcb611542f5fae13222cf3a
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-06 04:33:09 +0000

    Added image model and updated Wor and Blog models to have a one-to-one relationship with Image

--- pages/models.py
+++ pages/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-class Colours(models.Model):
+class Image(models.Model):
+    caption = models.CharField(max_length=255)
+    src = models.ImageField(upload_to="images/work_blog", default='images/work_blog/default.jpg')
+    
+    def __str__(self):
+        return self.caption
+
+class Colour(models.Model):
+    background_colour = models.CharField(max_length=30)
     colour = models.CharField(max_length=30)
     
     def __str__(self):
@@ -19,6 +27,11 @@ class Blog(models.Model):
     location = models.CharField(max_length=40)
     descriptions = ArrayField(models.TextField())
     links = ArrayField(models.URLField())
+    image = models.OneToOneField(
+        Image, 
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     
     def __str__(self):
         return self.title
@@ -29,6 +42,11 @@ class Work(models.Model):
     tools = models.ManyToManyField(Tool)
     descriptions = ArrayField(models.TextField())
     links = ArrayField(models.URLField())
+    image = models.OneToOneField(
+        Image, 
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     
     def __str__(self):
         return self.title
