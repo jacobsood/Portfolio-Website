113d1bc4fca2d00e32a0d18e82c7d78cfae0443b
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-06 11:30:30 +0000

    Use env variables for production settings DEBUG, ALLOWED_HOSTS

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -22,9 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if (os.getenv("DJANGO_DEBUG") == "True"):
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = ["3.105.120.10", "hrithviksood.me"]
+ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS")]
 
 # Application definition
 
@@ -122,9 +125,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
