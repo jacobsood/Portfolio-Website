e17d78d337cd39098bce71c904332f9821d65890
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-09 05:37:45 +0000

    Added context values to be rendered

--- pages/views.py
+++ pages/views.py
@@ -1,21 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import *
 
 def home(request):
-	context = {}
+	context = {
+	}
 	return render(request, 'pages/index.html', context)
 
 def about(request):
-	context = {}
+	context = {
+		'page': '- About',
+		'page_style': '#about.menu',
+	}
 	return render(request, 'pages/about.html', context)
 
 def work(request):
-	context = {}
+	work_object = get_list_or_404(Work)
+	context = {
+		'page': '- Work',
+		'page_style': '#work.menu',
+		'post_list': work_object,
+		'color': 'hotpink',
+		'background': 'blue',
+	}
 	return render(request, 'pages/work_blog.html', context)
 
 def blog(request):
-	context = {}
+	context = {
+		'page': '- Blog',
+		'page_style': '#blog.menu',
+	}
 	return render(request, 'pages/work_blog.html', context)
 
 def contact(request):
-	context = {}
+	context = {
+		'page': '- Contact',
+		'page_style': '#contact.menu',
+		"email": "hrithviksood1@gmail.com",
+		"linkedin": "https://www.linkedin.com/in/hrithvik-sood/",
+		"github": "https://github.com/hsoo6373/",
+		"instagram": "https://www.instagram.com/hrithvik_sood/",
+	}
 	return render(request, 'pages/contact.html', context)
