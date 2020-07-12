b54f8bc2c16e944a1624ec9de67bf88710123232
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-12 18:15:25 +0000

    Added get_column and get_column_cycle functions which retrieves the attributes of the current model object, and cycles to the next

--- pages/templatetags/pages_extras.py
+++ pages/templatetags/pages_extras.py
@@ -14,9 +14,16 @@ def cycle_db_array(user_list):
     array_index = 0
     
 @register.simple_tag
-def cycle_array():
+def get_column(column):
+    """Gets the element attribute at the current index"""
     global db_array, array_size, array_index
     element = db_array[array_index]
-    array_index = 0 if array_index + 1 >= array_size else array_index + 1
-    return element.shade
+    return getattr(element, column)
 
+@register.simple_tag
+def get_column_cycle(column):
+    """Gets the element attribute at the current index and cycles to the next position"""
+    attribute = get_column(column)
+    global array_size, array_index
+    array_index = 0 if array_index + 1 >= array_size else array_index + 1
+    return attribute
