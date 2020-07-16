ae0a4cd804ba756ca118e1a4c31cf6a0ded68dec
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-16 05:27:28 +0000

    Created abstracts

--- pages/models.py
+++ pages/models.py
@@ -1,27 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-class Video(models.Model):
-    title = models.CharField(max_length=255)
-    src = models.FileField(upload_to="videos", default="videos/default.mp4")
-    
-    def __str__(self):
-        return self.title
-
-class Image(models.Model):
-    caption = models.CharField(max_length=255)
-    src = models.ImageField(upload_to="images/work_blog", default='images/work_blog/ComingSoon.png')
-    
-    def __str__(self):
-        return self.caption
-        
-class Link(models.Model):
-    button_text = models.CharField(max_length=50, default="Visit Website")
-    url = models.URLField(default="www.hrithviksood.me")
-    
-    def __str__(self):
-        return self.button_text
-
 class Colour(models.Model):
     shade = models.CharField(max_length=30)
     background_colour = models.CharField(max_length=30)
@@ -30,41 +9,54 @@ class Colour(models.Model):
     def __str__(self):
         return self.shade
 
+class WorkBlogField(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    descriptions = models.TextField()
+    image = models.ImageField(upload_to="images/work_blog", default="images/work_blog/ComingSoon.png")
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        abstract = True
+        ordering = ['-date']
+
 class Tool(models.Model):
     tool = models.CharField(max_length=30)
     
     def __str__(self):
         return self.tool
 
-class Blog(models.Model):
-    title = models.CharField(max_length=255)
-    date = models.DateField(auto_now=False, auto_now_add=False)
+class Blog(WorkBlogField):
     location = models.CharField(max_length=40)
-    descriptions = ArrayField(models.TextField())
-    links = ArrayField(models.URLField())
-    image = models.OneToOneField(
-        Image, 
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
-    
-    def __str__(self):
-        return self.title
 
-class Work(models.Model):
-    title = models.CharField(max_length=255)
-    purpose = models.TextField()
+class Work(WorkBlogField):
+    purpose = models.CharField(max_length=255)
     tools = models.ManyToManyField(Tool)
-    descriptions = ArrayField(models.TextField())
-    links = ArrayField(models.URLField())
-    image = models.OneToOneField(
-        Image, 
-        on_delete=models.CASCADE,
-    )
-    video = models.OneToOneField(
-        Video,
+    video = models.FileField(upload_to="videos", default="videos/default.mp4")
+    
+class Link(models.Model):
+    button_text = models.CharField(max_length=50, default="Visit Website")
+    url = models.URLField(default="www.hrithviksood.me")
+        
+    class Meta:
+        abstract = True
+
+class BlogLink(Link):
+    blog = models.ForeignKey(
+        Blog,
         on_delete=models.CASCADE,
     )
     
     def __str__(self):
-        return self.title
+        return str(self.blog) + " - " + self.button_text
+        
+class WorkLink(Link):
+    work = models.ForeignKey(
+        Work,
+        on_delete=models.CASCADE,
+    )
+    
+    def __str__(self):
+        return str(self.work) + " - " + self.button_text
